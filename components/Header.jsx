7696345ae750afff8fa2eb7fd196f41d6b799453
Oluwadara-Oludare <dotundara91@gmail.com>
2024-10-16 14:49:44 +0100

    Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,14 +6,14 @@ const Header = () => {
     <div className="py-5 md:px-12 gap-2 lg:px-28 mt-9">
       <div className="flex  md:gap-5 lg:gap-9 justify-between items-center">
         <Image 
-          src="/assets/download.png" // Ensure this file exists in the 'public' folder
+          src="/assets/download.png" alt='' // Ensure this file exists in the 'public' folder
           width={100} 
           height={100} // Adjust height as necessary
           alt="Header Image"
           className="w-[40px] md:w-[60px] lg:w-[80px] " 
         />
         <input placeholder='Search' className= 'w-[200px] md:w-[300px] lg:w-[600px] ml--7  flex items-center gap-2 text-sm md:text-md lg:text-lg py-1 px-2 sm:py-3  border border-solid border-black '/>
-        <Image className='ml--9 w-[25px] md:w-[40px] lg:w-[40px]' width={40} height={10} src='/assets/notification.png'/>
+        <Image className='ml--9 w-[25px] md:w-[40px] lg:w-[40px]' width={40} height={10} src='/assets/notification.png' alt=''/>
         
       </div>
       
